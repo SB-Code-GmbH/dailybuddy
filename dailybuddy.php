8692fb8b31860a8a090d5b7531e40cf7b2778aef
Ilja Becker <ilja@beckercloud.online>
2025-12-06 14:28:01 +0100

    update 1.0.3

--- dailybuddy.php
+++ dailybuddy.php
@@ -4,7 +4,7 @@
  * Plugin Name: DailyBuddy
  * Plugin URI: https://dailybuddy.net/
  * Description: A modular collection of essential WordPress features that can be individually enabled or disabled. Includes tools for duplicating posts, maintenance mode, media organization, custom widgets, and Elementor extensions - all in one place.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Ilja Becker
  * Author URI: https://profiles.wordpress.org/beckerilja/
  * Text Domain: dailybuddy
